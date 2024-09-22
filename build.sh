554ebc5ab8d4bbe53c19f12aa14a70402285a1ad
Victor Rodriguez <awtGerry@gmail.com>
2024-09-22 08:38:27 -0600

    Try again!

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ gleam run -m lustre/dev build --minify
 mkdir -p ./priv/static/assets
 cp -r ./assets/* ./priv/static/assets/
 
-sed 's|priv/static/awtweb.css|awtweb.min.css|' ./index.html | sed 's|priv/static/awtweb.mjs|awtweb.min.mjs|' > priv/static/index.html
+# does not work with priv/static removing the priv/static prefix
+sed 's|/priv/static/awtweb.css|awtweb.min.css|' ./index.html | sed 's|/priv/static/awtweb.mjs|awtweb.min.mjs|' > priv/static/index.html
