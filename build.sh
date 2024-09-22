17433eeaada7733181acca4031329916f89e8876
Victor Rodriguez <awtGerry@gmail.com>
2024-09-21 23:32:25 -0600

    fix app.min.mjs to named project

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-gleam run -m lustre/dev build --minify
+gleam run -m lustre/dev build app --minify
 
 cp ./index.html ./priv/static/index.html
 
-sed -i 's/app\.css/app\.min\.css/' ./priv/static/index.html
-sed -i 's/app\.mjs/app\.min\.mjs/' ./priv/static/index.html
+sed -i 's/awtweb\.css/awtweb\.min\.css/' ./priv/static/index.html
+sed -i 's/awtweb\.mjs/awtweb\.min\.mjs/' ./priv/static/index.html
 sed -i 's/\/priv\/static/\./' ./priv/static/index.html
