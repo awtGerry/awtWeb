4c01541eead5730fded46d1598874b7d6f22e348
Victor Rodriguez <awtGerry@gmail.com>
2024-09-22 01:25:57 -0600

    renamed to project

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-gleam run -m lustre/dev -- build app --minify
+gleam run -m lustre/dev build --minify
 
 # icons
 mkdir -p ./priv/static/assets
 cp -r ./assets/* ./priv/static/assets/
 
-sed 's|priv/static/app.css|app.min.css|' ./index.html | sed 's|priv/static/app.mjs|app.min.mjs|' > priv/static/index.html
+sed 's|priv/static/awtweb.css|awtweb.min.css|' ./index.html | sed 's|priv/static/awtweb.mjs|awtweb.min.mjs|' > priv/static/index.html
